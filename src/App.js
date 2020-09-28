da7a20f070e15d3120f84dcd7b07c7d4373410a6
tuokaikyle <867367934@qq.com>
2020-09-28 15:29:17 +1000

    qt tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className="bg-blue-200">Hello World</h1>
     </div>
   );
 }
